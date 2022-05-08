2ad4d1daac9468b514237b6971b357c3a056efda
bojana87 <jbojana87@hotmail.com>
2022-05-08 15:39:01 +0200

    homework changes

--- hw12/zadatak2/index.php
+++ hw12/zadatak2/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,28 +8,30 @@
     <title>Kalkulator plocica</title>
     <link rel="stylesheet" href="zadatak2.css">
 </head>
+
 <body>
     <div class="container">
         <div class="form-wrapper">
             <h3>Kalkulator plocica</h3>
             <form method="POST" action="zadatak2.php" target="rezultat">
-            <label>Sirina zida (cm):
-                <input type="number" name="sirinazida" placeholder="sirina zida">
-            </label></br>
-            <label>Visina zida (cm):
-                <input type="number" name="visinazida" placeholder="visina zida">
-            </label></br>
-            <label>Sirina plocice (cm):
-                <input type="number" name="sirinaplocice" placeholder="sirina plocice">
-            </label></br>
-            <label>Visina plocice (cm):
-                <input type="number" name="visinaplocice" placeholder="visina plocice">
-            </label></br>
-            <input type="submit" name="submit" value="izracunaj" id="button"></input>    
-            </form></br>
+                <label>Sirina zida (cm):
+                    <input type="number" name="sirinazida" placeholder="sirina zida">
+                </label><br>
+                <label>Visina zida (cm):
+                    <input type="number" name="visinazida" placeholder="visina zida">
+                </label><br>
+                <label>Sirina plocice (cm):
+                    <input type="number" name="sirinaplocice" placeholder="sirina plocice">
+                </label><br>
+                <label>Visina plocice (cm):
+                    <input type="number" name="visinaplocice" placeholder="visina plocice">
+                </label><br>
+                <input type="submit" name="submit" value="izracunaj" id="button"></input>
+            </form><br>
             <iframe name="rezultat"></iframe>
         </div>
     </div>
-    
+
 </body>
+
 </html>
